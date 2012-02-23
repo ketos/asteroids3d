13fe80323f25365b6db19c9b925165d0e50d06f3
Henning <hstruebe@uos.de>
2012-02-23 13:16:34 +0100

    Sound auf Linuxrechnern/SDL_mixer lokal hinzugefügt

---
+++ src/ext/sound/lib/readmidi.d
@@ -0,0 +1,8 @@
+build/readmidi.lo: timidity/readmidi.c /usr/include/SDL/SDL_rwops.h \
+ /usr/include/SDL/SDL_stdinc.h /usr/include/SDL/SDL_config.h \
+ /usr/include/SDL/SDL_platform.h /usr/include/SDL/begin_code.h \
+ /usr/include/SDL/close_code.h /usr/include/SDL/SDL_error.h \
+ timidity/config.h /usr/include/SDL/SDL_config.h \
+ /usr/include/SDL/SDL_endian.h timidity/common.h timidity/instrum.h \
+ timidity/playmidi.h timidity/readmidi.h timidity/output.h \
+ timidity/ctrlmode.h
