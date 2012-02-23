a3a1c4f1c0ca40b8e4054072683e8fb99b346d51
tim <tim@tim-Studio-XPS-1640.(none)>
2012-02-23 15:02:33 +0100

    ja ohne sound.cpp kanns net klappen

---
+++ src/io/Sound.hpp
@@ -0,0 +1,26 @@
+#ifndef _SOUND_H
+#define _SOUND_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+ 
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+
+
+class Sound{
+public:
+    Sound(int c, const char *wav);
+    ~Sound();
+    void playWAV();
+    void playBackground();
+    void playMP3(const char *mp3);
+private:
+    int Channel;
+    Mix_Chunk *music;
+};
+
+#endif //_SOUND_H
