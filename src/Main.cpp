e71570eefceedcf04165f3c1d2509207e0891865
Michel <michel@michel-HP-625.(none)>
2012-02-24 16:34:24 +0100

    Ober cools Icon

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,10 +7,12 @@ using namespace std;
 #include "rendering/Bullet.hpp"
 #include "rendering/Fighter.hpp"
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon("res/images/icon.png"));
     MainWindow mainWindow;
     mainWindow.show();
  
