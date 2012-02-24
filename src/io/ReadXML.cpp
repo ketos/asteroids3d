772a5a0917c0be121ff05c9c4ad913e33471a034
Boeser <boeser@boeser-laptop.(none)>
2012-02-24 13:10:25 +0100

    docs in readXML hinzugefügt

--- src/io/ReadXML.cpp
+++ src/io/ReadXML.cpp
@@ -2,30 +2,37 @@
 
 vector<std::pair<glVector<float>*, glVector<float>* >* >  ReadXML::readConfig (string& filename)
 {
-	vector<std::pair<glVector<float>*, glVector<float>* >* > m_asteroidList;
+	 // m_asteroidList speichert alle einglesenen Asteroiden
+	 vector<std::pair<glVector<float>*, glVector<float>* >* > m_asteroidList;
 	
- 	QDomDocument doc("level");
-    //QFile file("config.xml");
+ 	 QDomDocument doc("level");
+ 	 
+ // Fehlerbehandlung 	 
+  /*QFile file("config.xml");
     
-    //if(!file.open(QIODevice::ReadOnly))
-    //{
+    if(!file.open(QIODevice::ReadOnly))
+    {
         
-    //}    
-    //if( !doc.setContent(&file))
-    //{
-    //    file.close();
+    }    
+    if( !doc.setContent(&file))
+    {
+        file.close();
  
-    //}
-    //file.close();
-    
+    }
+    file.close();
+  */
     glVector<float>* pos; 
     glVector<float>* axis;
     std::pair<glVector<float>*,glVector<float>*>* p;
     
-    QDomNode level   = doc.documentElement();
+    QDomNode level       = doc.documentElement();
     QDomNode asteroid    = level.firstChild();
     QDomElement ad;
     
+    /**
+    * ein Asteroid wird durchlaufen, alle seine Attribute in floats umgewandelt
+    * und in m_asteroidList eingefügt
+    */ 
     while(!asteroid.isNull())
     {
         ad = asteroid.toElement();
@@ -35,7 +42,7 @@ vector<std::pair<glVector<float>*, glVector<float>* >* >  ReadXML::readConfig (s
                                    ad.attribute("pos_z","").toFloat());
                        
         axis = new glVector<float>(ad.attribute("axis_x","").toFloat(),
-                        	       ad.attribute("axis_y","").toFloat(),
+                        	        ad.attribute("axis_y","").toFloat(),
                                    ad.attribute("axis_z","").toFloat());               
         p->first   = pos;
         p->second  = axis;
