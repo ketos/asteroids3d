7f9f0b5c8e9fca29926847680a5ac2ee4820a642
Henning <henning@henning-1001PXD.(none)>
2012-02-27 16:38:34 +0100

    Asteoriden Radius und Hitbox angepasst

--- src/rendering/Mini_Asteorid.cpp
+++ src/rendering/Mini_Asteorid.cpp
@@ -10,7 +10,7 @@
 Mini_Asteorid::Mini_Asteorid(glVector<float> start_position, glVector<float> flight_axis)
 :     Asteorid(start_position, flight_axis)
 {
-
+	radius = 100;
 }
 
 void Mini_Asteorid::run()
