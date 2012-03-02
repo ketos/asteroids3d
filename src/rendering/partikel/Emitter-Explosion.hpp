7bf85e31b4464942d3387f9f0c2378075071296e
MEGAGOTT <aloha@aloha-Laptop.(none)>
2012-03-02 18:40:38 +0100

    Emitter-Explosion fertig

--- src/rendering/partikel/Emitter-Explosion.hpp
+++ src/rendering/partikel/Emitter-Explosion.hpp
@@ -1,27 +1,56 @@
+/**
+ * @file Emitter-Explosion.hpp
+ *
+ * @author gruppe3
+ */
 #ifndef __EMITTER_EXPLOSION_H
 #define __EMITTER_EXPLOSION_H
 
 #include "rendering/partikel/Partikel-Explosion.hpp"
 #include <list>
 
+/**
+ * @brief Erzeugt und verwaltet die Partikel der Explosion
+ */
 class EmitterExplosion
 {
 public:
+    /**
+     * @brief Konstruktor
+     */
     EmitterExplosion();
-    EmitterExplosion(int Partikelanzahl);
+
+    /**
+     * @brief Destruktor
+     */
     ~EmitterExplosion();
 
-    int getaktPartikel();
+    /**
+     * @brief Fügt ein Partikel zur Liste hinzu, solange die maximale Partikelanzahl
+     *          noch nicht erreicht ist
+     * 
+     * @param partikel das neue Explosionspartikel
+     * @return ob es hinzugefügt werden konnte oder nicht
+     */
     bool add(PartikelExplosion partikel);
+    
+    /**
+     *  @brief updatet alle aktuell vorhandenen Partikel
+     */
     void update();
 
+    /**
+     * @brief erzeugt Partikel für eine Explosion
+     *
+     * @param a_pos Position der Explosion / des zerstörten Asteoriden
+     */
     void createPartikel(glVector<float> a_pos);
-    void setMaxPartikel();
     
 private:
+    /**
+     * @brief Liste mit den Partikel
+     */
     std::list<PartikelExplosion> m_partikel;
-    int m_PartikelZahl;
-    unsigned int m_maxPartikel;
 };
 
 #endif // __EMITTER_EXPLOSION_H
